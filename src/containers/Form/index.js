d707f9490b7854d28ef9d040fae594a514448e88
marie <127097512+marie-bcc@users.noreply.github.com>
2023-12-15 13:39:20 +0100

    update test home page

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -1,10 +1,12 @@
+// @ts-nocheck
 import { useCallback, useState } from "react";
 import PropTypes from "prop-types";
 import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
+// eslint-disable-next-line no-promise-executor-return
+const mockContactApi = () => new Promise((resolve) => resolve(true));
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
@@ -16,7 +18,7 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
-        onSuccess(); 
+        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
@@ -57,11 +59,11 @@ const Form = ({ onSuccess, onError }) => {
 Form.propTypes = {
   onError: PropTypes.func,
   onSuccess: PropTypes.func,
-}
+};
 
 Form.defaultProps = {
   onError: () => null,
   onSuccess: () => null,
-}
+};
 
 export default Form;
